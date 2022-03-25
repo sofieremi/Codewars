653bad1956b868b148f464828ee8caf22ce9bf56
sofieremi <seremina910@gmail.com>
2022-03-26 00:01:57 +1000

    python

---
+++ text.py
@@ -0,0 +1,30 @@
+a = 'On the other hand, we denounce with righteous indignation and dislike ' \
+    'men who are so beguiled and demoralized by the charms of pleasure of ' \
+    'the moment, so blinded by desire, that they cannot foresee the pain and' \
+    ' trouble that are bound to ensue; and equal blame belongs to those who fail in ' \
+    'their duty through weakness of will, which is the same as saying through shrinking from' \
+    ' toil and pain. These cases are perfectly simple and easy to distinguish. In free hour,' \
+    ' when our power of choice is untrammelled and when nothing prevents our being able to do' \
+    ' what we like best, every pleasure is to be welcomed and every pain avoided. But in certain ' \
+    'circumstances and owing to the claims of duty or the obligations of business it will frequently ' \
+    'occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always' \
+    ' holds in these matters to this principle of selection: he rejects pleasures to secure' \
+    ' other greater pleasures, or else he endures pains to avoid worse pains.'
+# b = a
+# a = a.split()
+# a = set(a)
+# max_len = 0
+# word = ''
+# for i in a:
+#     if len(i) > max_len:
+#         max_len = len(i)
+#         word = i
+# print(word)
+# c = {i: b.count(i) for i in a}
+# vv = 0
+# k_max = ''
+# for k, v in c.items():
+#     if vv < v:
+#         vv = v
+#         k_max = k
+# print(vv, k_max)
